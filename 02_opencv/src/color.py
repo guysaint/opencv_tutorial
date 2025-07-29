c724c5c80b3c500f464509baab7ce880d2f9b7b4
guysaint <guysaint@naver.com>
2025-07-29 10:14:59 +0900

    color tutorial 기본 세팅

--- 02_opencv/src/color.py
+++ 02_opencv/src/color.py
@@ -4,6 +4,18 @@ import numpy as np
 #기본 값
 img = cv2.imread('../img/like_lenna.png' )
 
-cv2.imshow('default',img)
+#bgr
+bgr = cv2.imread('../img/like_lenna.png',cv2.IMREAD_COLOR)
+
+#a
+bgra = cv2.imread('../img/like_lenna.png',cv2.IMREAD_UNCHANGED)
+
+# shape
+print("default", img.shape, "color", bgr.shape, "unchanged", bgra.shape)
+
+cv2.imshow('img',img)
+cv2.imshow('bgr', bgr)
+cv2.imshow('bgr', bgra[:,:,3])
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
