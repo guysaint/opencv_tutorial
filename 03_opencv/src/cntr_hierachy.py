332fe798c9aa4ab130ca8f9776592462e4b2b348
guysaint <guysaint@naver.com>
2025-07-30 11:30:48 +0900

    TREE에서만 3이 출력되도록 수정

--- 03_opencv/src/cntr_hierachy.py
+++ 03_opencv/src/cntr_hierachy.py
@@ -25,17 +25,15 @@ print(len(contour2), hierarchy)
 cv2.drawContours(img, contour, 0, (0,255,0), 3)
 
 #모든 컨투어 그리기
-
-
-
 for idx, cont in enumerate(contour2):
     #랜덤한 컬러 추출
-    if idx == 3:
-        color = [int(i) for i in np.random.randint(0,255,3)]
-        #컨투어 인덱스 마다 랜덤한 색상으로 그리기
-        cv2.drawContours(img2, contour2, idx, color, 3)
-        #컨투어 첫 좌표에 인덱스 숫자 표시
-        cv2.putText(img2, str(idx), tuple(cont[0][0]), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,255))
+    #if idx == 3: #3번만 표시하도록 -> idx == 3 일때만 적용돼서 contour에 컨투어 그리기가 한 번만 적용됨
+    color = [int(i) for i in np.random.randint(0,255,3)]
+    #컨투어 인덱스 마다 랜덤한 색상으로 그리기
+    #cv2.drawContours(img2, contour2, idx, color, 3)
+    cv2.drawContours(img2, contour2, 3, color, 3) # -> idx를 3으로 고정시켜서 TREE에서는 항상 3만 출력되도록 함.
+    #컨투어 첫 좌표에 인덱스 숫자 표시
+    cv2.putText(img2, str(idx), tuple(cont[0][0]), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,255))
 
 #화면 출력
 cv2.imshow('RETR_EXTERNAL', img)
