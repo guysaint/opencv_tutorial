f9ec26401b5ffabd86e36a9a612916bcb0dee7b6
guysaint <guysaint@naver.com>
2025-07-30 15:40:43 +0900

    히스토그램 그래프가 너무 작고 수치를 시각화 하기 위해 matplotlib을 사용한 코드로 수정

--- 03_opencv/src/line_tracer.py
+++ 03_opencv/src/line_tracer.py
@@ -1,27 +1,42 @@
 import cv2
-import matplotlib.pylab as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
 cap = cv2.VideoCapture(1) 
-            # 1번 카메라 장치 연결 ---①
-if cap.isOpened():                      # 캡쳐 객체 연결 확인
+
+plt.ion() #실시간 플롯을 위해 인터렉티브 모드 on
+           
+if cap.isOpened():    
     while True:
         ret, img = cap.read()
         if ret:
             #BGR -> GRAY
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
             #그레이 히스토그램 계산
             hist = cv2.calcHist([gray], [0], None, [256], [0,256])
-            cv2.normalize(hist, hist, 0, 100, cv2.NORM_MINMAX)
+            #cv2.normalize(hist, hist, 0, 100, cv2.NORM_MINMAX)
+            #화면 출력
+            cv2.imshow('camera(gray)', gray)   # GRAY로 카메라 표시
+            
+            #Matplotlib 히스토그램
+            plt.cla()
+            plt.plot(hist, color='black')
+            plt.title('Gray Histogram')
+            plt.xlabel('Pixel Value (0~255)')
+            plt.ylabel('Frequency')
+            plt.xlim([0, 255])
+            plt.pause(0.01)
 
+
+            '''
             #히스토그램 시각화용 이미지 생성
             hist_img = np.full((100,256), 255, dtype=np.uint8)
             for x,y in enumerate(hist):
                 cv2.line(hist_img, (x, 100), (x, 100-int(y)), 0)
-
-            #화면 출력
-            cv2.imshow('camera(gray)', gray)   # GRAY로 카메라 표시
-            cv2.imshow('histogram(gray)', hist_img)
+            '''
+            
+            
 
             #hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) #BGR -> HSV로 변환
             #cv2.imshow('camera', hsv) #HSV로 카메라 표시
