59f8d9401c24698816f9b0febae0ac4d4d3295b3
guysaint <guysaint@naver.com>
2025-07-30 16:01:38 +0900

    ROI(관심영역)을 사용하여 선과 배경 픽샐을 분포화하고 비교하는 코드 추가

--- 03_opencv/src/line_tracer.py
+++ 03_opencv/src/line_tracer.py
@@ -13,19 +13,55 @@ if cap.isOpened():
             #BGR -> GRAY
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+            # 스캔라인 ROI 설정(세로 10px 범위, 중앙 수평선 부근)
+            roi = gray[240:250, :]
+
+            # ROI를 절반으로 나눔
+            h, w = roi.shape
+            left_roi = roi[:, :w//2] #배경 영역
+            right_roi = roi[:, w//2:] #선 추정 영역
+
+            # 각각의 히스토그램 계산
+            hist_left = cv2.calcHist([left_roi], [0], None, [256], [0, 256])
+            hist_right = cv2.calcHist([right_roi], [0], None, [256], [0, 256])
+
+            cv2.normalize(hist_left, hist_left, 0, 1, cv2.NORM_MINMAX)
+            cv2.normalize(hist_right, hist_right, 0, 1, cv2.NORM_MINMAX)
+
+            #디버깅용 ROI 시각 표시
+            cv2.line(img, (0, 240), (img.shape[1], 240), (255, 0, 0), 2)
+            cv2.line(img, (0, 249), (img.shape[1], 249), (255, 0, 0), 2)
+            cv2.imshow('camera(gray)', gray)
+            
+            # 검정색(0~50) 구간의 총 빈도 비교
+            black_left = np.sum(hist_left[:50])
+            black_right = np.sum(hist_right[:50])
+
+            print(f"Black Pixel (Left ROI): {black_left:.4f}")
+            print(f"Black Pixel (Right ROI): {black_right:.4f}")
+            black_pixels = np.sum((right_roi < 50) & (right_roi > 0))  # 0은 순수 배경 노이즈일 수 있음
+            total_pixels = right_roi.size
+            print(f"Black pixel ratio in Right ROI: {black_pixels / total_pixels:.4f}")
+
+
+
+            #ROI에 검정선이 실제로 들어오는지 확인
+            cv2.imshow('ROI view', roi)
             #그레이 히스토그램 계산
-            hist = cv2.calcHist([gray], [0], None, [256], [0,256])
+            #hist = cv2.calcHist([gray], [0], None, [256], [0,256])
             #cv2.normalize(hist, hist, 0, 100, cv2.NORM_MINMAX)
             #화면 출력
-            cv2.imshow('camera(gray)', gray)   # GRAY로 카메라 표시
+            #cv2.imshow('camera(gray)', gray)   # GRAY로 카메라 표시
             
             #Matplotlib 히스토그램
             plt.cla()
-            plt.plot(hist, color='black')
-            plt.title('Gray Histogram')
+            plt.plot(hist_left, color='green', label='Left ROI (Background)')
+            plt.plot(hist_right, color='black', label='Right ROI (Line)')
+            plt.title('Gray Histogram(ROI-based)')
             plt.xlabel('Pixel Value (0~255)')
-            plt.ylabel('Frequency')
+            plt.ylabel('Normalized Frequency')
             plt.xlim([0, 255])
+            plt.legend()
             plt.pause(0.01)
 
 
