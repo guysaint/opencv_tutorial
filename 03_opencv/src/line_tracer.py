43d59172e793845d3c10a02ab3e2a02b68b9b3a4
guysaint <guysaint@naver.com>
2025-07-30 14:37:25 +0900

    s를 누르면 사진찍고 img폴더에 저장

--- 03_opencv/src/line_tracer.py
+++ 03_opencv/src/line_tracer.py
@@ -7,12 +7,14 @@ if cap.isOpened():                      # 캡쳐 객체 연결 확인
         ret, img = cap.read()
                    # 다음 프레임 읽기
         if ret:
-            # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #BGR -> GRAY 변환
-            # cv2.imshow('camera', gray)   # GRAY로 카메라 표시
-            hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) #BGR -> HSV로 변환
-            cv2.imshow('camera', hsv) #HSV로 카메라 표시
-            if cv2.waitKey(1)  & 0xFF == ord('q'):    # 1ms 동안 키 입력 대기 ---②
-                break                   # 아무 키라도 입력이 있으면 중지
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #BGR -> GRAY 변환
+            cv2.imshow('camera', gray)   # GRAY로 카메라 표시
+            #hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) #BGR -> HSV로 변환
+            #cv2.imshow('camera', hsv) #HSV로 카메라 표시
+            if cv2.waitKey(1)  & 0xFF == ord('q'):
+                break
+            if cv2.waitKey(1) == ord("s"):            # s키를 누르면
+                cv2.imwrite('../img/photo.jpg', img)    #phto.jpg로 사진 저장
         else:
             print('no frame')
             break
