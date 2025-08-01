a749e074ed38f0e45e2b95cc1b163a463bd35ce5
guysaint <guysaint@naver.com>
2025-08-01 10:01:58 +0900

    스켈레톤 코드 업데이트

--- 05_opencv/src/qr_scan.py
+++ 05_opencv/src/qr_scan.py
@@ -3,3 +3,10 @@ import matplotlib.pyplot as plt
 import pyzbar.pyzbar as pyzbar
 
 
+img = cv2.imread('../img/frame.png')
+
+plt.imshow(img)
+plt.show()
+#cv2.imshow('qr_code', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
