7ac4e62046a64c6dc064966f6ec1bbaead49e794
guysaint <guysaint@naver.com>
2025-08-01 10:40:59 +0900

    qr코드 그레이스케일 적용

--- 05_opencv/src/qr_scan.py
+++ 05_opencv/src/qr_scan.py
@@ -2,11 +2,13 @@ import cv2
 import matplotlib.pyplot as plt
 import pyzbar.pyzbar as pyzbar
 
-
 img = cv2.imread('../img/frame.png')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-plt.imshow(img)
+plt.imshow(gray)
 plt.show()
 #cv2.imshow('qr_code', img)
+
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
