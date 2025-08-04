0dfe362f158485de0b1f0b8e83aec6f247d3cfea
guysaint <guysaint@naver.com>
2025-08-04 14:56:12 +0900

    웹캠에서 작동하도록 수정

--- 06_opencv/src/track_bgsub_mog.py
+++ 06_opencv/src/track_bgsub_mog.py
@@ -1,12 +1,13 @@
 import numpy as np, cv2
 
-cap = cv2.VideoCapture('../img/walking.avi')
+#cap = cv2.VideoCapture('../img/walking.avi')
+cap = cv2.VideoCapture(1)
 fps = cap.get(cv2.CAP_PROP_FPS)
 delay = int(1000/fps)
 # cv2.createBackgroundSubtractorMOG2(history, varThreshold, detectShadows)
 # history: 과거 프레임의 갯수, 배경을 학습하는데 얼마나 많은 프레임을 기억할지
 # varThreshold: 필셀이 객체인 배경인지 구분하는 기준값
-fgbg = cv2.createBackgroundSubtractorMOG2(50, 100, detectShadows=False)
+fgbg = cv2.createBackgroundSubtractorMOG2(50, 30, detectShadows=False)
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
