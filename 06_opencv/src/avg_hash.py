c2d1e5395b7abc3d8346b94266c1fe69b599ec42
guysaint <guysaint@naver.com>
2025-08-04 10:20:48 +0900

    평균 해시로 변환하는 실습 코드

--- 06_opencv/src/avg_hash.py
+++ 06_opencv/src/avg_hash.py
@@ -1,8 +1,27 @@
 import cv2
 
 img = cv2.imread('../img/pistol.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+gray = cv2.resize(gray, (16,16))
+
+# 영상의 평균값 구하기
+avg = gray.mean()
+
+# 영상의 평균값을 기준으르로 0과 1로 변환
+bin = 1* (gray > avg)
+print(bin)
+
+# 2진수 문자열을 16진수 문자열로 변환
+dhash=[]
+for row in bin.tolist():
+    s = ''.join([str(i) for i in row])
+    dhash.append('%02x'%(int(s,2)))
+dhash = ''.join(dhash)
+print(dhash)
 
 
+cv2.namedWindow('pistol', cv2.WINDOW_GUI_NORMAL)
 cv2.imshow('pistol', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
