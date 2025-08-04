e209b4f8e63c85b929887a02a522ceabc059cad5
guysaint <guysaint@naver.com>
2025-08-04 15:47:46 +0900

    track_optical_fameback.py 실행 되도록 수정: np.int -> int

--- 06_opencv/src/track_optical_farneback.py
+++ 06_opencv/src/track_optical_farneback.py
@@ -6,14 +6,14 @@ import cv2, numpy as np
 def drawFlow(img,flow,step=16):
   h,w = img.shape[:2]
   # 16픽셀 간격의 그리드 인덱스 구하기 ---②
-  idx_y,idx_x = np.mgrid[step/2:h:step,step/2:w:step].astype(np.int)
+  idx_y,idx_x = np.mgrid[step/2:h:step,step/2:w:step].astype(int)
   indices =  np.stack( (idx_x,idx_y), axis =-1).reshape(-1,2)
   
   for x,y in indices:   # 인덱스 순회
     # 각 그리드 인덱스 위치에 점 그리기 ---③
     cv2.circle(img, (x,y), 1, (0,255,0), -1)
     # 각 그리드 인덱스에 해당하는 플로우 결과 값 (이동 거리)  ---④
-    dx,dy = flow[y, x].astype(np.int)
+    dx,dy = flow[y, x].astype(int)
     # 각 그리드 인덱스 위치에서 이동한 거리 만큼 선 그리기 ---⑤
     cv2.line(img, (x,y), (x+dx, y+dy), (0,255, 0),2, cv2.LINE_AA )
 
